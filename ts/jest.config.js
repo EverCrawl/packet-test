3c44dd92d2140e44605682cc4eb4dab542c5e644
jprochazk <honza.spacir@gmail.com>
2021-01-16 09:50:49 +0100

    fix naming

--- ts/jest.config.js
+++ ts/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     transform: {
         "^.+\\.ts?$": "ts-jest",
     },
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$",
+    testRegex: "(/__tests__/.*|(\\.|/)(spec))\\.ts?$",
     testEnvironment: "node",
     moduleFileExtensions: ["ts", "js"],
     moduleDirectories: ["node_modules", "src"],
