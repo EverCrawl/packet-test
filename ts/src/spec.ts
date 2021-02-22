08a167968f4bd4862c864eebe8b98142927ce270
jprochazk <honza.spacir@gmail.com>
2021-02-22 14:52:55 +0100

    update test to match new ts codegen + disable rust test

--- ts/src/spec.ts
+++ ts/src/spec.ts
@@ -1,55 +1,53 @@
 import { Reader, Writer } from "packet";
-import { read, write, ComplexType, Flag } from "./test";
+import { ComplexType, Flag } from "./test";
+
+let data = new ComplexType(
+    ["first", "second"],
+    [{ x: 0.0, y: 1.0 }],
+    Flag.B,
+    [{
+        a: 0,
+        b: 1,
+        c: 30,
+        d: 100,
+    }],
+    0,
+    undefined,
+    Flag.A
+);
+
+let bin = new Uint8Array([
+    2, 0, 0, 0, // names.len()
+    5, 0, 0, 0, // names[0].len()
+    102, 105, 114, 115, 116, // names[0]
+    6, 0, 0, 0, // names[1].len()
+    115, 101, 99, 111, 110, 100, // names[1]
+    1, 0, 0, 0, // positions.len()
+    0, 0, 0, 0, // positions[0].x
+    0, 0, 128, 63, // positions[0].y
+    2,  // flag
+    1, 0, 0, 0, // values.len()
+    0, 0, 0, 0, // values[0].a
+    1, 0, 0, 0,   // values[0].b
+    30,  // values[0].c
+    100, // values[0].d
+    1,   // opt == Some
+    0,   // opt
+    0,   // opt_s == None
+    1,   // opt_e == Some
+    1,   // opt_e
+]).buffer;
 
 describe("generated", function () {
-    it("works", function () {
-        let data: ComplexType = {
-            names: ["first", "second"],
-            positions: [{ x: 0.0, y: 1.0 }],
-            flag: Flag.B,
-            values: [{
-                a: 0,
-                b: 1,
-                c: 30,
-                d: 100,
-            }],
-            opt: 0,
-            opt_e: Flag.A
-        };
-        let bin = new Uint8Array([
-            2, 0, 0, 0, // names.len()
-            5, 0, 0, 0, // names[0].len()
-            102, 105, 114, 115, 116, // names[0]
-            6, 0, 0, 0, // names[1].len()
-            115, 101, 99, 111, 110, 100, // names[1]
-            1, 0, 0, 0, // positions.len()
-            0, 0, 0, 0, // positions[0].x
-            0, 0, 128, 63, // positions[0].y
-            2,  // flag
-            1, 0, 0, 0, // values.len()
-            0, 0, 0, 0, // values[0].a
-            1, 0, 0, 0,   // values[0].b
-            30,  // values[0].c
-            100, // values[0].d
-            1,   // opt == Some
-            0,   // opt
-            0,   // opt_s == None
-            1,   // opt_e == Some
-            1,   // opt_e
-        ]).buffer;
-        {
-            let writer = new Writer();
-            write(writer, data);
-            expect(writer.finish()).toEqual(bin);
-        }
+    it("reads ComplexType", function () {
+        expect(ComplexType.read(bin)).toEqual(data);
+    })
+    it("writes ComplexType - prealloc", function () {
+        let buffer = new ArrayBuffer(bin.byteLength);
+        expect(data.write(buffer)).toEqual(bin);
 
-        {
-            let reader = new Reader(bin);
-            // @ts-ignore
-            let actual: ComplexType = {};
-            read(reader, actual);
-            expect(actual).toEqual(data);
-        }
-
+    });
+    it("writes ComplexType - no prealloc", function () {
+        expect(data.write()).toEqual(bin);
     });
 });
